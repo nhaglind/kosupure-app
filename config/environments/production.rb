f01f1344f0be211a4afa2eb599fc549bfbc11632
nhaglind <nhaglind@gmail.com>
2016-04-19 13:27:39 -0500

    Fix syntax error on produciton.rb that might have been causing failure of heroku deployment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,3 @@ Rails.application.configure do
    # Required for Devise. Remember to change localhost:3000 to actual application host
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
-end
