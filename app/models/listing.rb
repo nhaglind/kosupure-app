f86661460a0a056626e443c0dd8be3e325e0c659
nhaglind <nhaglind@gmail.com>
2016-04-19 14:05:26 -0500

    Added validation for users and listings.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,4 @@
 class Listing < ActiveRecord::Base
-	validates :name, :description, presence: true
-  validates :price, presence:true,
-    numericality: true
 
   if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
@@ -13,4 +10,8 @@ class Listing < ActiveRecord::Base
   end
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_presence :image
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+
 end
