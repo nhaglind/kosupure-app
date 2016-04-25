1b0d323cc584dad8069ad0b8ca9cbbdb141ef330
nhaglind <nhaglind@gmail.com>
2016-04-25 11:09:50 -0500

    Pulled in all carrierwave changes, small fix on order listing, and the postgres switch.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424005944) do
+ActiveRecord::Schema.define(version: 20160420165759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,10 @@ ActiveRecord::Schema.define(version: 20160424005944) do
     t.string   "name"
     t.text     "description"
     t.decimal  "price"
+    t.json     "images"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
-    t.json     "images"
   end
 
   create_table "orders", force: :cascade do |t|
