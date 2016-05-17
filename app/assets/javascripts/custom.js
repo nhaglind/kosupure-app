d10b92df10a9c4c328fb948059892f3b1f3777cd
nhaglind <nhaglind@gmail.com>
2016-05-17 13:12:00 -0500

    wrap all pages in container with padding, clean up manage inventory page, add pattern, fix button placements.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,5 @@
+/* Hide Featured Banner */
+
 jQuery(document).ready(function(){
         jQuery('.hide-hero-anime').on('click', function(event) {        
              jQuery('.jumbotron').toggle('show');
