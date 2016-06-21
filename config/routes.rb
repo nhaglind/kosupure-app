0a7cec43e8b63e197317818f50f804b7436bc345
nhaglind <nhaglind@gmail.com>
2016-06-21 18:21:45 -0500

    really delete active admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
 
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
-
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   resources :listings do
     resources :orders, only: [:new, :create]
