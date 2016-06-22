63ff34041a61ed34c168559a386a488fedb0d491
nhaglind <nhaglind@gmail.com>
2016-06-21 19:19:20 -0500

    uh ssl

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,6 @@ class PagesController < ApplicationController
 
   def letsencrypt
     # use your code here, not mine
-    render text: "nD6NAh6KikCPDvsyGuStVPcMJH61Jugkpcy0OPHg0og.FU9mpYT9y1uj4yK2md2hbR-vxR27z2bCtf691yw8Odg"
+    render text: "0hQv5ZVn5cNyxwTwYwU4vH9_lO9VFdrw4WbDI0hhe2o.FU9mpYT9y1uj4yK2md2hbR-vxR27z2bCtf691yw8Odg"
   end
 end
