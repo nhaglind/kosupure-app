5dc22337a5ca396683a35b5f0afc7583ef33108b
tpanneck <travis.panneck@gmail.com>
2016-06-22 12:26:33 -0500

    add production config for action mailer thru gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,23 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
    # Required for Devise. Remember to change localhost:3000 to actual application host
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'kosupureio.herokuapp.com' }
+
+  # Email settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["gmail_domain"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["gmail_username"],
+    password: ENV["gmail_password"]
+
+  }
+
 end
