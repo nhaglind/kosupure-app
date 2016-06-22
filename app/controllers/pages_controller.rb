2fea866b117d4d5904612bf683e86a1ab6b0058e
nhaglind <nhaglind@gmail.com>
2016-06-21 20:07:11 -0500

    ssl

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,6 @@ class PagesController < ApplicationController
 
   def letsencrypt
     # use your code here, not mine
-    render text: "#{params[:id]}."
+    render text: "#{params[:id]}.FU9mpYT9y1uj4yK2md2hbR-vxR27z2bCtf691yw8Odg"
   end
 end
