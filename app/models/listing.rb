6d1ed1df213e7f683a7f652a098d62e82f3e3a55
tpanneck <travis.panneck@gmail.com>
2016-06-26 01:51:56 -0500

    add image size validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,10 @@ class Listing < ActiveRecord::Base
 
   validates :name, :description, :price, :category_id, presence: true
   validates :price, :quantity, numericality: { greater_than: 0 }
+
   validates_length_of :images, maximum: 5, too_long: ": maximum number of images is 5"
+  # validates :validate_images, file_size: { less_than_or_equal_to: 1.megabyte }
+  validate :validate_images
 
   belongs_to :user
   belongs_to :category
@@ -16,6 +19,14 @@ class Listing < ActiveRecord::Base
   scope :ordering, -> (order) { reorder(order) }
   scope :named, -> (name) { where(user: name) }
   # scope :paging, ->  (page) { paginate(:page => page, :per_page => 12) }
+
+  def validate_images
+    images.each do |image|
+      if image.present?
+        errors.add(:images, "cannot exceed 1 megabyte") if image.size >= 1000000
+      end
+    end
+  end
 
   def to_param
   	[id, name.parameterize].join("-")
