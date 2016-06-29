5406a55564bd3d36968c447dae13a6681f7f75b3
nhaglind <nhaglind@gmail.com>
2016-06-28 19:35:36 -0500

    more email changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["gmail_domain"],
+    # domain: ENV["gmail_domain"],
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["gmail_username"],
